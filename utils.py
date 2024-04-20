08c1304d8186ac5daf28f46dc80bbfdeb2b4622a
Ashique <ashique@Ashiques-MacBook-Pro.local>
2024-04-20 15:58:00 +0530

    Adding....

--- utils.py
+++ utils.py
@@ -30,7 +30,28 @@ def get_llama_completion(prompt,model="llama2"):
         return response.json().get("response","--ERROR--")
     return False
 
+def download_llama():
+    import torch
+    from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+
+    model_id =  "NousResearch/Llama-2-7b-hf"
+    # model_id = "meta-llama/Llama-2-7b-chat-hf"
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16
+    )
+
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id, 
+        quantization_config=bnb_config, 
+        device_map="auto")
+
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "right"
+
 
 if __name__ == "__main__":
-    test = get_llama_completion("Am i learnig fast enough to chase llm")
-    print(test)
+    download_llama()
