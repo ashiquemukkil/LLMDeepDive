422d2dd46a239151e765896e0db837bc13bbc7af
Ashique <ashique@Ashiques-MacBook-Pro.local>
2024-04-28 10:07:34 +0530

    adding..

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import openai
 from openai import OpenAI
 import requests
 
-OLLAMA_URL = "http://localhost:11434/api/generate"
+OLLAMA_URL = "http://localhost:11434/api/generate" 
 
 os.environ["OPENAI_API_KEY"] = "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"
 
@@ -29,28 +29,6 @@ def get_llama_completion(prompt,model="llama2"):
     if response.status_code == 200:
         return response.json().get("response","--ERROR--")
     return False
-
-def download_llama():
-    import torch
-    from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
-
-    model_id =  "NousResearch/Llama-2-7b-hf"
-    # model_id = "meta-llama/Llama-2-7b-chat-hf"
-    bnb_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_use_double_quant=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.bfloat16
-    )
-
-    model = AutoModelForCausalLM.from_pretrained(
-        model_id, 
-        quantization_config=bnb_config, 
-        device_map="auto")
-
-    tokenizer = AutoTokenizer.from_pretrained(model_id)
-    tokenizer.pad_token = tokenizer.eos_token
-    tokenizer.padding_side = "right"
 
 
 if __name__ == "__main__":
